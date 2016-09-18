8ea5c5ba514fe9796edf6ece8f4932021142574e
Alexashooo <aleksandar.sunjic@gmail.com>
2016-09-18 21:18:38 +0200

    Game is finished

--- scripting.js
+++ scripting.js
@@ -36,8 +36,7 @@
 	// movement and the speed of the ball at te same time
    var movement_step_ball = 6; 
 	
-   // start angle
-	     
+   // start angle     
    var ax = 0; //angle corection x-axis
    var ay = 0; //angle corection y-axis
 
@@ -56,7 +55,7 @@
          }
     };
 
-    //result
+    //show result
     var player_score = 0; 
     var com_score = 0; 
  
@@ -70,7 +69,6 @@
     show_player_score.style.top = '50px';
 
 
-
     var show_com_score = document.getElementById("com");
     show_com_score.style.color = "red";
     show_com_score.style.fontSize = "900%";
@@ -79,13 +77,40 @@
     show_com_score.style.position = "absolute";
     show_com_score.style.left = '571px';
     show_com_score.style.top = '50px';
- 
 
-    //d.style.left = x_pos+'px';
-    //d.style.top = y_pos+'px';
-    
-   
- 
+
+
+    //messages to the player
+    var message_win = document.getElementById("msg_win");
+    message_win.style.color = "red";
+    message_win.style.fontSize = "200%";
+    message_win.style.fontWeight = "500";
+    message_win.style.position = "absolute";
+    message_win.style.left = '301px';
+    message_win.style.top = '250px';
+    message_win.style.visibility = "hidden";
+
+    var message_lose = document.getElementById("msg_lose");
+    message_lose.style.color = "red";
+    message_lose.style.fontSize = "200%";
+    message_lose.style.fontWeight = "500";
+    message_lose.style.position = "absolute";
+    message_lose.style.left = '161px';
+    message_lose.style.top = '250px';
+    message_lose.style.visibility = "hidden";
+
+    var onload_greeting = document.getElementById("onload_greeting");
+    onload_greeting.style.color = "red";
+    onload_greeting.style.fontSize = "300%";
+    onload_greeting.style.fontWeight = "500";
+    onload_greeting.style.position = "absolute";
+    onload_greeting.style.left = '121px';
+    onload_greeting.style.top = '250px';
+
+
+    onLoadGreeting  = function(){
+         onload_greeting.style.visibility = "visible";
+    }
 
 	
 	 // Ball object
@@ -99,6 +124,7 @@
          this.y_direction = 1;
          this.x_direction = 1;
          this.line_width = 1;
+         this.angle = 0;
          var current_y_position = 0;
          
 	     this.render = function() {
@@ -114,6 +140,8 @@
 	     
 	     this.move = function(){	
              
+            if(com_score < 11 && player_score < 11){ 
+                
 	     	 		//COLLISION WITH THE TABLE TOP
 	     	 		//left to right
 	     			 if(this.y_position - this.ball_radius - 1 === 20 && this.x_direction > 0 && this.y_direction < 0) {
@@ -222,8 +250,6 @@
              
              
              
-             
-             
                       //collision with right(com) paddle
                     //top to down
                     else if(this.x_position + this.ball_radius + 1 === 1005 && this.y_position >= com.y_position && this.y_position <= com.y_position + 1 + com.paddle_length + 1 && this.x_direction > 0 && this.y_direction > 0) {
@@ -263,25 +289,32 @@
              
                     // serve again
                    
-                    else if(this.x_position - 1 - this.ball_radius  <= 36 && (this.y_position < player.y_position || this.y_position > player.y_position + 1 + player.paddle_length + 1)) {
-                          com_score++;
-                          show_com_score.innerHTML = com_score;
-                          serve_counter--;  
-                          
-                    }
-                   
-             
-                    else if(this.x_position >= 1000 - this.ball_radius - 1  && (this.y_position < com.y_position || this.y_position > com.y_position + 1 + com.paddle_length + 1)) {
-                          player_score++;
-                          show_player_score.innerHTML = player_score;
-                          serve_counter--; 
-                          
-                    }
                  
-                    else {
+                    else if(this.x_position - 1 - this.ball_radius  <= 36 && (this.y_position < player.y_position || this.y_position > player.y_position + 1 + player.paddle_length + 1)) {
+                            com_score++;
+                            show_com_score.innerHTML = com_score;
+                            serve_counter--; 
+                            if(com_score === 11){
+                                message_lose.style.visibility = "visible";
+                            }  
+                    }
+                    
+                    else if(this.x_position >= 1000 - this.ball_radius - 1  && (this.y_position < com.y_position || this.y_position > com.y_position + 1 + com.paddle_length + 1)) {
+                            player_score++;
+                            show_player_score.innerHTML = player_score;
+                            serve_counter--; 
+                            if(player_score === 11){
+                                message_win.style.visibility = "visible";
+                            }
+                    }
+                        
+            
+                    
+                       
+                    
                     //FREE MOVEMENTS
                     // free movement from left to right and up
-                        if(this.y_position - this.ball_radius - 1 - movement_step_ball * ay >= 20 && this.x_position + this.ball_radius + 1 + movement_step_ball * ax <= 1005 && this.x_direction > 0 && this.y_direction < 0){
+                        else if(this.y_position - this.ball_radius - 1 - movement_step_ball * ay >= 20 && this.x_position + this.ball_radius + 1 + movement_step_ball * ax <= 1005 && this.x_direction > 0 && this.y_direction < 0){
                             this.x_position += movement_step_ball * ax;
                             this.y_position -= movement_step_ball * ay;
                             current_y_position = this.y_position;
@@ -311,17 +344,66 @@
                             current_x_position = this.x_position;
                         }
                         
-                    }
-                        
+           }
+           else {
+             com_score = 0;
+             player_score = 0;
+             show_com_score.innerHTML = com_score;
+             show_player_score.innerHTML=player_score;
+             start_the_game = 1;
+           }
 	     };
-	     
+        
+        
+        this.random_speed_angle = function(){
+            random_number = Math.random();
+            if(random_number > 0 && random_number<= 0.33){
+                this.angle = 30;
+                movement_step_ball = 6.5;
+                return this.angle;
+            }
+            else if(random_number > 0.33 && random_number<= 0.66){
+                this.angle = 45;
+                movement_step_ball = 6;
+                return this.angle;
+            }
+            else if(random_number > 0.66 && random_number<= 1){
+                this.angle = 60;
+                movement_step_ball = 5;
+                return this.angle;
+            }    
+            
+        }
+        
+	   
 	     this.serve_the_ball = function() {
-               reflection_angle(45);
+               //reflection_angle(45);
+               reflection_angle(this.random_speed_angle());
                this.x_position=500;
                this.y_position=500;
                render();
-               this.y_direction = Math.abs(this.y_direction);
-               this.x_direction = Math.abs(this.x_direction);
+             
+              random_direction = Math.random();
+              if(random_direction > 0 && random_direction <= 0.25){
+                 this.y_direction = Math.abs(this.y_direction);
+                 this.x_direction = Math.abs(this.x_direction);
+                  
+              }
+              else if(random_direction > 0.25 && random_direction <= 0.5){
+                 this.y_direction = - Math.abs(this.y_direction);
+                 this.x_direction = Math.abs(this.x_direction);
+              }
+             
+              else if(random_direction > 0.5 && random_direction <= 0.75){
+                 this.y_direction = Math.abs(this.y_direction);
+                 this.x_direction = - Math.abs(this.x_direction);
+              }
+             
+              else if(random_direction > 0.75 && random_direction <= 1){
+                 this.y_direction =  - Math.abs(this.y_direction);
+                 this.x_direction = - Math.abs(this.x_direction);
+              }
+                  
 	     };
 	     
 	};
@@ -353,7 +435,7 @@
  	
  	var player = new Paddle(30, 300, 5, 100);
  	var com = new Paddle(1005, 300, 5, 100);
- 	var ball = new Ball(700, 500, 5);
+ 	var ball = new Ball(600, 350, 5);
  	var table = new Table(20, 20, 1000, 600);
   
 
@@ -397,10 +479,6 @@ Paddle.prototype.comMove = function() {
 };
 
 
-
-
-
- 	
  
  // event listener for Paddle...
  
@@ -429,25 +507,38 @@ Paddle.prototype.comMove = function() {
 
 
  var serve_counter = 0;
+ var start_the_game = 1;
 
  
- var step = function() {	   
-         if(serve_counter===0){
-             //window.addEventListener("keydown", function(event) { 
-                 //if(event.keycode == 32 || event.which == 32) { 
-                    ball.serve_the_ball();
-                    serve_counter++;  
-                 //} 
-             //});
-         }
-         else{
-            com.comMove();
-            ball.move();
-            render(); 
-         };
-          	   	
-  	   	requestAnimationFrame(step);   
+ var step = function() {
+         render();
+         if(start_the_game === 1){         
+                window.addEventListener("keydown", function(event) { 
+                    if(event.keycode === 32 || event.which === 32) {
+                           start_the_game = 0;
+                           onload_greeting.style.visibility = "hidden";
+                           message_lose.style.visibility = "hidden";
+                           message_win.style.visibility = "hidden";
+                           movement_step_ball=6;
+                    }
+                });
+                
+          }
+          else if(serve_counter === 0 && start_the_game === 0){
+                ball.serve_the_ball();
+                serve_counter++;
+          }
+     
+          else if(serve_counter > 0 && start_the_game === 0){
+                com.comMove();
+                ball.move();
+          }
+       	   	
+  	   	requestAnimationFrame(step);  
+    
   };
+
+
 
  //refreshing window akka animation
  	
